dc46f30295ab61ad001b3e0adeacc032fcecf61e
toejough <toejough@gmail.com>
2016-06-04 21:17:20 -0400

    moved some app logic up out of the semantic layer

--- otter/api.py
+++ otter/api.py
@@ -1,11 +1,8 @@
 """
-Library API.
+API Layer.
 
-This is how the user should interact with the library.
-
-The intent is to be a simple-to-use overlay of the purely functional application code.
-The translation is from easy use to required actions for each function.
-State and some simple logic is allowed.
+Translation from how I want users to be able to use the library to the
+core actions performed.
 """
 
 
@@ -32,16 +29,7 @@ class Stream:
 
         # set interactors
         self._write = functools.partial(
-            app.write,
-            write_interactor=write_interactor,
-            recorder_interactor=recorder_interactor,
-        )
-        self._output_ends_with = functools.partial(
-            app.output_ends_with,
-            recorder_interactor=recorder_interactor,
-        )
-        self._reset = functools.partial(
-            app.reset,
+            app.write_from_stream,
             write_interactor=write_interactor,
             recorder_interactor=recorder_interactor,
         )
@@ -53,23 +41,14 @@ class Stream:
         Rewrite data from a new line if the stream's data is not the last output
         recorded.
         """
-        # if the last recorded output matches
-        if self._output_ends_with(self.data):
-            # write the new data
-            output = self._write(data)
-        else:
-            # reset the writer if there's really data to write
-            if data:
-                self._reset()
-            # write the old data, then the new data
-            output = self._write(self.data + data)
+        output = self._write(self.data, data)
         # update the stream data
         self.data += data
         # return what the writer returned
         return output
 
 
-def replace(parent, func_name, write_interactor, *, recorder_interactor=mem_recorder):
+def replace(parent, func_name, *, write_interactor=stdout_writer, recorder_interactor=mem_recorder):
     """watch the output."""
     # get functions replaced in the given parent
     replaced = STATE.get(parent, {})
@@ -80,11 +59,15 @@ def replace(parent, func_name, write_interactor, *, recorder_interactor=mem_reco
     # get the original function
     func = getattr(parent, func_name)
     # replace the function
-    app.replace(
-        parent, func_name, write_interactor,
-        recorder_interactor=recorder_interactor
+    setattr(
+        parent, func_name,
+        functools.wraps(func)(functools.partial(
+            app.write_interruption,
+            write_interactor=write_interactor,
+            recorder_interactor=recorder_interactor,
+        ))
     )
-    # save teh original and the replacement
+    # save the original and the replacement
     replaced[func_name] = {
         'original': func,
         'replacement': replaced
@@ -95,5 +78,5 @@ def replace(parent, func_name, write_interactor, *, recorder_interactor=mem_reco
 
 def replace_stds():
     """replace the std streams."""
-    replace(sys.stdout, 'write', stdout_writer)
-    replace(sys.stderr, 'write', stderr_writer)
+    replace(sys.stdout, 'write', write_interactor=stdout_writer)
+    replace(sys.stderr, 'write', write_interactor=stderr_writer)
--- otter/app.py
+++ otter/app.py
@@ -1,49 +1,75 @@
 """
-The How of the application.
+Semantic actions, core to the library.
 
-These are pure functions which call the interactors to do work.
+In Clean Architecture terms - the business logic.
 """
 
 
 import functools
 
 
-def output_ends_with(data, *, recorder_interactor):
-    """return the remembered data."""
-    return data and recorder_interactor.output_record.endswith(data)
-
-
-def write(data, *, write_interactor, recorder_interactor):
+def _write(data, *, from_stream=False, write_interactor, recorder_interactor):
     """write & record the given data."""
     output = write_interactor.write(data)
-    recorder_interactor.record(data, from_stream=True)
+    recorder_interactor.record(data, from_stream=from_stream)
     return output
 
 
-def reset(*, write_interactor, recorder_interactor):
+def _reset(*, write_interactor, recorder_interactor):
     """reset the output state so that a new stream is obviously new."""
     if not write_interactor.is_reset(recorder_interactor.output_record):
         write_interactor.reset()
 
 
-def replace(parent, func_name, replacement_writer, *, recorder_interactor):
-    """watch the output from the function."""
-    @functools.wraps(getattr(parent, func_name))
-    def wrapped(data):
-        """wrapped interruption writer."""
-        # if stream data was printed last and there is new data to print
-        prior_data = recorder_interactor.output_record
-        if (
-            data and
-            prior_data and
-            recorder_interactor.last_from_stream and
-            not replacement_writer.is_reset(prior_data)
-        ):
-            # reset the output
-            replacement_writer.reset()
-        # write and record the non-stream output
-        output = replacement_writer.write(data)
-        recorder_interactor.record(data, from_stream=False)
-        return output
+def write_from_stream(prior_stream_data, data, *, write_interactor, recorder_interactor):
+    """
+    Write data from a stream.
 
-    setattr(parent, func_name, wrapped)
+    Takes the prior stream data, the new data, and write/record interactors.
+
+    Returns the return from the write_interactor.
+    """
+    # set up the write function
+    write = functools.partial(
+        _write,
+        from_stream=True,
+        write_interactor=write_interactor,
+        recorder_interactor=recorder_interactor
+    )
+    # if the last recorded output matches
+    if prior_stream_data and recorder_interactor.output_record.endswith(prior_stream_data):
+        # write the new data
+        output = write(data)
+    else:
+        # reset the writer if there's really data to write
+        if data:
+            _reset(
+                write_interactor=write_interactor,
+                recorder_interactor=recorder_interactor
+            )
+        # write the old data, then the new data
+        output = write(prior_stream_data + data)
+    # return what the writer returned
+    return output
+
+
+def write_interruption(data, *, write_interactor, recorder_interactor):
+    """interruption writer."""
+    # if stream data was printed last and there is new data to print
+    if (
+        data and
+        # recorder_interactor.output_record and
+        recorder_interactor.last_from_stream
+    ):
+        # reset the output
+        _reset(
+            write_interactor=write_interactor,
+            recorder_interactor=recorder_interactor
+        )
+    # write and record the non-stream output
+    return _write(
+        data,
+        from_stream=False,
+        write_interactor=write_interactor,
+        recorder_interactor=recorder_interactor
+    )
