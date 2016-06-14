3cc7985f335648997577a7166d64f7f52617f559
toejough <toejough@gmail.com>
2016-06-13 20:00:41 -0400

    updating layering in output.py

--- otter/interactors/output.py
+++ otter/interactors/output.py
@@ -17,6 +17,8 @@ class Output:
         self._flush = writer.flush
         self._recorder = mem_recorder.get_recorder()
 
+    # [ Public ]
+    # [ -Internal ]
     def write(self, data, *, from_stream=False):
         """write the data to stdout."""
         output = self._write(data)
@@ -26,13 +28,15 @@ class Output:
 
     def reset(self):
         """reset the output."""
-        if not self._recorder.is_reset():
+        if not self._is_reset():
             self.write('\n')
 
+    # [ -Args ]
     def combine(self, prior_data, new_data):
         """combine two hunks of data."""
         return prior_data + new_data
 
+    # [ -Recorder ]
     def last_output_matches(self, given_output):
         """last output matches."""
         return self._recorder.last_output_matches(given_output)
@@ -40,3 +44,9 @@ class Output:
     def last_from_stream(self):
         """Return whether the last output was from a stream."""
         return self._recorder.last_from_stream
+
+    # [ Private ]
+    # [ -Recorder ]
+    def _is_reset(self):
+        """return whether the output is reset."""
+        return self._recorder.is_reset()
