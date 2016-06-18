ad27ab2f3039c877f7daf9d7b55312194eb7af78
toejough <toejough@gmail.com>
2016-06-18 14:38:29 -0400

    simplified output device

--- otter/interactors/output.py
+++ otter/interactors/output.py
@@ -22,9 +22,7 @@ class OutputDevice:
     def reset(self):
         """Reset the device."""
         if not self._is_reset:
-            self._write('\n')
-            self._flush()
-            self._is_reset = True
+            self.old_write('\n')
 
     def old_write(self, data):
         """Write data to the device via the given output mechanism."""
@@ -40,64 +38,23 @@ class OutputDevice:
         if stream_data and not last_output_matches:
             self.reset()
         if last_output_matches:
-            stream_data = stream_data[len(self._last_output):]
-        self.old_write(stream_data)
+            to_write = stream_data[len(self._last_output):]
+        else:
+            to_write = stream_data
+        self.old_write(to_write)
         self._last_output = stream_data
         self._last_from_stream = True
 
     # [ Private ]
-    # [ -App ]
-    def _should_reset_before_interruption(self, data, last_from_stream):
-        """return whether we should reset the output."""
-        return data and last_from_stream
-
     # [ -Interactor ]
-    def _last_output_from_stream(self):
-        """Return whether the last output was from a stream."""
-        return self._last_from_stream
-
     def write_interruption(self, data):
         """Actually write the data."""
         # Data
-        last_from_stream = self._last_output_from_stream()
-        should_reset = self._should_reset_before_interruption(data, last_from_stream)
+        last_from_stream = self._last_from_stream
 
-        if should_reset:
+        if data and last_from_stream:
             self.reset()
         output = self.old_write(data)
         self._last_output = data
         self._last_from_stream = False
         return output
-
-
-class StdOutOutputMechanism:
-    """The stdout output mechanism."""
-
-    _write = sys.stdout.write
-    _flush = sys.stdout.flush
-
-    def write(self, data):
-        """write the data."""
-        self._write(data)
-        self._flush()
-
-
-class StdErrOutputMechanism:
-    """The stderr output mechanism."""
-
-    _write = sys.stderr.write
-    _flush = sys.stderr.flush
-
-    def write(self, data):
-        """write the data."""
-        self._write(data)
-        self._flush()
-
-
-class Recorder:
-    """Record output data."""
-
-    def __init__(self, initial_data):
-        """init state."""
-        self.last_output = initial_data
-        self.last_from_stream = False
